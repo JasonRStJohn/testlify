ca76b3052a8ca6109029620951b43f1f51996aed
JasonRStJohn <jasonrstjohn@gmail.com>
2019-05-29 14:22:20 -0400

    sp issue

--- src/components/HomeText.vue
+++ src/components/HomeText.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <h2>At Infinity Graphics we specialize in tayloring your print or web experience to your needs</h2>
+        <h2>At Infinity Graphics we specialize in tailoring your print or web experience to your needs</h2>
         <p>
             From logos and fliers to web base ui/ux designed to accomplish your goals. 
             With years of experience with print and web we do the footwork ourselves and don't outsource. This results in a focused end product that is faster, more efficient, and this more profitable.
