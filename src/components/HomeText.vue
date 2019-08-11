b19a212e3871dc0b9872201f4bb33970279e36bc
JasonRStJohn <jasonrstjohn@gmail.com>
2019-08-11 14:57:05 -0400

    Typo

--- src/components/HomeText.vue
+++ src/components/HomeText.vue
@@ -3,7 +3,7 @@
         <h2>At Infinity Graphics we specialize in tailoring your print or web experience to your needs</h2>
         <p>
             From logos and fliers to web base ui/ux designed to accomplish your goals. 
-            With years of experience with print and web we do the footwork ourselves and don't outsource. This results in a focused end product that is faster, more efficient, and this more profitable.
+            With years of experience with print and web we do the footwork ourselves and don't outsource. This results in a focused end product that is faster, more efficient, and thus more profitable.
         </p>
     </main>
 </template>
